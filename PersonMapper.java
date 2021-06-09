a72da27ef4665fe52e033ad0b0b3358bec0481c0
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2021-06-09 12:09:29 -0300

    Add files via upload

---
+++ PersonMapper.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.personapi.mapper;
+
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
+
+
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy" )
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+}
